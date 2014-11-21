8c8588a5517663513b133ce6057647c5546dd2f6
Md Khan <khan4019@gmail.com>
2014-11-21 16:48:58 -0600

    testing files

--- demo/console.js
+++ demo/console.js
@@ -1,5 +1,26 @@
+var jsDude = { name: 'that js dude',
+					favLib: 'herald washington library',
+					favDrink: 'water'
+				};
+
+var awesomeDude = { name: 'Mr. awesome',
+					favLib: 'awesome library',
+					favDrink: 'beer'
+				};
+
+var regularDude = { name: 'Who cares',
+					favLib: 'hates reading',
+					favDrink: 'meetup beer'
+				};
 
 
+//1. log multiple
+
+
+//2. log multiple objects
+
+
+//3. add this. http://stackoverflow.com/questions/10224691/output-multi-dimensional-arrays
 function getNames(developerData){
 	var names =[];
 
